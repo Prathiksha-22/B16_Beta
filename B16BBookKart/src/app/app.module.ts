1acd2d87b817579c20e4efae6f2a4fa89d4ae29d
Prathiksha-22 <68772027+Prathiksha-22@users.noreply.github.com>
2021-11-22 02:14:24 -0700

    productList initial commit
    -the list of products have been displayed with hover option

--- B16BBookKart/src/app/app.module.ts
+++ B16BBookKart/src/app/app.module.ts
@@ -3,14 +3,27 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProductlistComponent } from './productlist/productlist.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatListModule } from '@angular/material/list';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductlistComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatCardModule,
+    MatIconModule,
+    MatButtonModule,
+    MatListModule
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
